076789dcb0bc0e3080add4462ebed052a464413a
LINLJN <linlijun3e@gmail.com>
2019-11-24 03:52:40 +0100

    24-11-2019

--- Proyecto.R
+++ Proyecto.R
@@ -3,15 +3,21 @@
 PIB <- read.csv('dat/PIB/variacion_anual_del_pib_d.csv',sep = ";",header = T,encoding = 'UTF-8')
 PIB$Variaci?n.anual.del.PIB <- as.numeric(gsub(',','.',PIB$Variaci?n.anual.del.PIB))
 
+
+
 #Las previsiones sobre el PIB espa?ol de la OCDE
 
 pib <-  read.csv('dat/PIB/las_previsiones_sobre_el_PIB.csv',sep = ";",header = T,encoding = 'UTF-8')
 pib$Variaci?n.interanual.del.PIB <- as.numeric(gsub(',','.',pib$Variaci?n.interanual.del.PIB))
 
+
+
 #Lectura de datos de poblacion de Espana Generico
 
 poblacion <-  read.csv('dat/Poblacion/evolucion_de_la_poblacion.csv',sep = ";",header = T,encoding = 'UTF-8')
 poblacion$Evoluci?n.de.la.poblaci?n.residente.en.Espa?a <-as.numeric(gsub(',','.',poblacion$Evoluci?n.de.la.poblaci?n.residente.en.Espa?a))
+
+
 
 #--------------------------------------
 p.poblacion <- read.csv('dat/Poblacion/prevision_poblacion.csv',sep = ";",header = T,encoding = 'UTF-8')
@@ -22,6 +28,8 @@ sub <- function(x){
 
 p.poblacion[,3:5] <- lapply(p.poblacion[,3:5],sub)
 
+
+
 #--------------------------------------
 emigracion <-  read.csv('dat/Poblacion/evolucion_de_la_emigracio.csv',sep = ";",header = T,encoding = 'UTF-8')
 
@@ -29,6 +37,9 @@ emigracion <-  read.csv('dat/Poblacion/evolucion_de_la_emigracio.csv',sep = ";",
    x<-as.numeric(gsub("\\.","",x))
   }
 emigracion[,3:4] <- lapply(emigracion[,3:4],mil)
+
+
+
 
 
 #Lectura de poblacion por comunidad autonoma 
@@ -47,6 +58,55 @@ p.total <- p.total[-c(1,20:24),]
 library(reshape2)
 p.total <- melt(p.total,"X")
 p.total$variable <- gsub('X','',p.total$variable)
+names (p.total)
+names(p.total) = c('Provincia','year','poblacion')
+
+library(tidyverse)
+library(lubridate)
+p.total %>%
+  select(Provincia,poblacion,year) %>%
+  group_by(year) %>%
+  arrange(year,-poblacion) %>%
+  mutate(rank = 1:n()) %>%
+  filter(rank <= 19) ->
+ranked_by_year1  
+
+ranked_by_year1$year<- as.integer(ranked_by_year1$year)
+ranked_by_year1$poblacion<- as.integer(ranked_by_year1$poblacion)
+
+ggplot(data = ranked_by_year1) +
+  aes(group = poblacion, fill = Provincia) +
+  aes(xmin = 0 ,
+      xmax = poblacion / 1000000) +
+  aes(ymin = rank - .45,
+      ymax = rank + .45) +
+  scale_y_reverse() +
+  scale_x_continuous(
+    limits = c(-350, 1400),
+    breaks = c(0, 400, 800, 1200),
+    labels = c(0, 400, 800, 1200)) +
+  labs(fill = "") +
+  geom_rect(alpha = .7) +
+  labs(x = 'Population (millions)') +
+  aes(label = Provincia, y = rank) +
+  geom_text(col = "gray13",
+            hjust = "right",
+            x = -50) +
+  labs(y = "") +
+  scale_fill_viridis_d(option = "magma",
+                       direction = -1) +
+  geom_text(x = 1000 , y = -10,
+            family = "Times",
+            aes(label = as.character(year)),
+            size = 30, col = "grey18") +
+  my_theme ->
+  my_plot
+  
+library(gganimate)
+options(gganimte.nframes = 20)
+my_plot + gganimate::transition_time(year)
+
+
 
 
 p.hombres <-  autonoma[,c(25:47)]
--- test.R
+++ test.R
@@ -1,74 +1,103 @@
-library(tidyverse)
-library(gganimate)
-library(gapminder)
-theme_set(theme_classic())
+autonoma <-  read.csv('dat/Poblacion/2853sc.csv',sep = ";",skip = 5)
 
-gap <- gapminder %>%
-  filter(continent == "Asia") %>%
+mil <- function(x){
+  x<-as.numeric(gsub("\\.","",x))
+}
+
+autonoma[,2:70] <- lapply(autonoma[,2:70],mil)
+
+p.total <- autonoma[,c(1:24)]
+p.total <- p.total[-c(1,20:24),]
+
+
+library(reshape2)
+p.total <- melt(p.total,"X")
+p.total$variable <- gsub('X','',p.total$variable)
+names (p.total)
+names(p.total) = c('Provincia','year','poblacion')
+
+library(tidyverse)
+library(lubridate)
+p.total %>%
+  select(Provincia,poblacion,year) %>%
   group_by(year) %>%
-  # The * 1 makes it possible to have non-integer ranks while sliding
-  mutate(rank = min_rank(-gdpPercap) * 1) %>%
-  ungroup()
+  arrange(year,-poblacion) %>%
+  mutate(rank = 1:n()) %>%
+  filter(rank <= 19) ->
+  ranked_by_year1  
 
-p <- ggplot(gap, aes(rank, group = country, 
-                     fill = as.factor(country), color = as.factor(country))) +
-  geom_tile(aes(y = gdpPercap/2,
-                height = gdpPercap,
-                width = 0.9), alpha = 0.8, color = NA) +
-  
-  # text in x-axis (requires clip = "off" in coord_*)
-  # paste(country, " ")  is a hack to make pretty spacing, since hjust > 1 
-  #   leads to weird artifacts in text spacing.
-  geom_text(aes(y = 0, label = paste(country, " ")), vjust = 0.2, hjust = 1) +
-  
-  coord_flip(clip = "off", expand = FALSE) +
-  scale_y_continuous(labels = scales::comma) +
-  scale_x_reverse() +
-  guides(color = FALSE, fill = FALSE) +
-  
-  labs(title='{closest_state}', x = "", y = "GFP per capita") +
-  theme(plot.title = element_text(hjust = 0, size = 22),
-        axis.ticks.y = element_blank(),  # These relate to the axes post-flip
-        axis.text.y  = element_blank(),  # These relate to the axes post-flip
-        plot.margin = margin(1,1,1,4, "cm")) +
-  
-  transition_states(year, transition_length = 4, state_length = 1) +
-  ease_aes('cubic-in-out')
-
-animate(p, fps = 25, duration = 20, width = 800, height = 600)
+ranked_by_year1$year<- as.integer(ranked_by_year1$year)
+ranked_by_year1$poblacion<- as.integer(ranked_by_year1$poblacion)
 
 
 
+install.packages("hrbrthemes")
+library(hrbrthemes)
 
-library(tidyverse)
+
+p <- ranked_by_year1 %>%
+  ggplot(aes(x = -rank,y = poblacion, group = poblacion)) +
+  geom_tile(aes(y = poblacion / 2, height = poblacion, fill = Provincia), width = 0.9) +
+  geom_text(aes(label = Provincia), hjust = "right", colour = "black", fontface = "bold", nudge_y = -100000) +
+  geom_text(aes(label = scales::comma(poblacion)), hjust = "left", nudge_y = 100000, colour = "grey30") +
+  coord_flip(clip="off") +
+  #scale_fill_manual(name = 'Provincia', values = c("#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3")) +
+  scale_fill_viridis_d(option = "magma",
+                       direction = -1)+
+  scale_x_discrete("") +
+  scale_y_continuous("",labels=scales::comma) +
+  hrbrthemes::theme_ipsum(plot_title_size = 32, subtitle_size = 24, caption_size = 20, base_size = 20) +
+  theme(panel.grid.major.y=element_blank(),
+        panel.grid.minor.x=element_blank(),
+        legend.position = c(0.4, 0.2),
+        plot.margin = margin(1,1,1,2,"cm"),
+        axis.text.y=element_blank()) +
+  # gganimate code to transition by year:
+  transition_time(year) +
+  ease_aes('cubic-in-out') +
+  labs(title='Largest Cities in the United States',
+       subtitle='Population in {round(frame_time,0)}',
+       caption='Source: United States Census
+michaeltoth.me / @michael_toth')
+
+animate(p, nframes = 750, fps = 25, end_pause = 30, width = 800, height = 800)
+
+
+
+
+
+
+
+
+
+
+
+my_theme <- ggplot(data = ranked_by_year1) +
+  aes(x = -rank,group = poblacion, fill = Provincia) +
+  aes(xmin = 0 ,
+      xmax = poblacion / 1000000) +
+  aes(ymin = rank - .45,
+      ymax = rank + .45) +
+  scale_y_discrete() +
+  scale_x_continuous("",labels=scales::comma) +
+  labs(fill = "") +
+  geom_tile(aes(y = poblacion / 2, height = poblacion, fill = Provincia), width = 0.9)+
+  geom_rect(alpha = .7) +
+  labs(x = 'Population (millions)') +
+  aes(label = Provincia, y = rank) +
+  geom_text(col = "gray13",
+            hjust = "right",
+            x = -50) +
+  labs(y = "") +
+  scale_fill_viridis_d(option = "magma",
+                       direction = -1) +
+  geom_text(x = 1000 , y = -10,
+            family = "Times",
+            aes(label = as.character(year)),
+            size = 30, col = "grey18") 
+
+   my_plot <- my_theme
+
 library(gganimate)
-library(gapminder)
-theme_set(theme_classic())
-
-
-a <- ggplot(p.total, aes(group = X, 
-                     fill = as.factor(X), color = as.factor(X))) +
-  geom_tile(aes(y = value/2,
-                height = value,
-                width = 0.9), alpha = 0.8, color = NA) +
-  
-  # text in x-axis (requires clip = "off" in coord_*)
-  # paste(country, " ")  is a hack to make pretty spacing, since hjust > 1 
-  #   leads to weird artifacts in text spacing.
-  geom_text(aes(y = 0, label = paste(X, " ")), vjust = 0.2, hjust = 1) +
-  
-  coord_flip(clip = "off", expand = FALSE) +
-  scale_y_continuous(labels = scales::comma) +
-  scale_x_reverse() +
-  guides(color = FALSE, fill = FALSE) +
-  
-  labs(title='aa', x = "", y = "yy") +
-  theme(plot.title = element_text(hjust = 0, size = 22),
-        axis.ticks.y = element_blank(),  # These relate to the axes post-flip
-        axis.text.y  = element_blank(),  # These relate to the axes post-flip
-        plot.margin = margin(1,1,1,4, "cm")) +
-  
-  transition_states(year, transition_length = 4, state_length = 1) +
-  ease_aes('cubic-in-out')
-
-animate(a, fps = 25, duration = 20, width = 414, height = 600)
+options(gganimte.nframes = 20)
+my_plot + gganimate::transition_time(year)
